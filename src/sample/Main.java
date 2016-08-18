47a5a8ad45f23c68a6a226207429cc90ba216210
Jianan Zhang <superzjn@gmail.com>
2016-08-17 21:15:42 -0500

    UI update

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -12,8 +12,8 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.setTitle("Stock Checker");
+        primaryStage.setScene(new Scene(root, 500, 300));
         primaryStage.show();
     }
 
