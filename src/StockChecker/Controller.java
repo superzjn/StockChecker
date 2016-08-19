affec8c1c9ade760126b6085a6a8306967b6884f
superzjn <superzjn@gmail.com>
2016-08-19 16:41:32 -0500

    Showall implementation

--- src/StockChecker/Controller.java
+++ src/StockChecker/Controller.java
@@ -3,6 +3,11 @@ package StockChecker;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
 
 public class Controller {
     @FXML
@@ -11,14 +16,63 @@ public class Controller {
     private TextArea outputUrls;
     @FXML
     private Label msgBox;
+    @FXML
+    private TextField searchBox;
+
+    private MySQLConnector sqlConnector;
+    private String sql = null;
 
     public void showAll() {
-        msgBox.setText("All urls are shown.");
+
+        outputUrls.clear();
+        sqlConnector = new MySQLConnector();
+        sql = "SELECT * FROM watchlist";
+        ResultSet resultSet = null;
+        int count = 0;
+
+        try {
+            resultSet = sqlConnector.query(sql);
+
+            while (resultSet.next()) {
+                outputUrls.appendText(resultSet.getString(1) + "\n");
+            }
+
+            // Get count
+            resultSet.last();
+            count = resultSet.getRow();
+
+            sqlConnector.close();
+            resultSet.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        msgBox.setText("Total of " + count + " urls are shown.");
     }
 
 
     public void addUrl() {
 
+        sqlConnector = new MySQLConnector();
+        int count = 1;
+
+        String input = inputUrls.getText();
+        String[] urls = input.split("[\\r\\n]+");     // make it works for Windows, UNIX and Mac, and ignore empty lines
+
+        for (String str : urls) {
+            try {
+                str = str.trim();    //Remove spaces
+                sql = "INSERT INTO watchlist VALUES('" + str + "',now());";
+                if (sqlConnector.update(sql)) {
+                    msgBox.setText(count + " urls are added.");
+                    count++;
+                }
+            } catch (Exception e) {
+                msgBox.setText("Update Failed.");
+                e.printStackTrace();
+            } finally {
+                sqlConnector.close();
+            }
+        }
     }
 
     public void delUrl() {
@@ -26,6 +80,10 @@ public class Controller {
     }
 
     public void checkStock() {
+
+    }
+
+    public void searchUrl() {
 
     }
 
--- src/StockChecker/MySQLConnector.java
+++ src/StockChecker/MySQLConnector.java
@@ -1,8 +1,6 @@
 package StockChecker;
 
 import java.sql.*;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 
 /**
  * Created by jzhang9 on 8/18/2016.
@@ -10,46 +8,72 @@ import java.util.logging.Logger;
 public class MySQLConnector {
 
     private Connection connection = null;  // Database connection
-    private PreparedStatement pst = null;  // SQL statement
+    private PreparedStatement preparedStatement = null;  // SQL statement
 
-    private static final String drivername = "com.mysql.jdbc.Driver";
+    private static final String driverName = "com.mysql.jdbc.Driver";
     private static final String url = "jdbc:mysql://localhost:3306/stockchecker";  // Database location
     private static final String user = "root";      // username
     private static final String password = "root";   // password
 
     public MySQLConnector() {
         try {
-            Class.forName(drivername);// 指定连接类型
+            Class.forName(driverName);//  Specify connection type
             connection = DriverManager.getConnection(url, user, password);
-
-            pst = connection.prepareStatement("select * from watchlist");
-            ResultSet rs = pst.executeQuery();
-            while (rs.next()) {
-                System.out.println(rs.getString(1));
-            }
         } catch (Exception e) {
+            System.out.println("Failed to Establish Databse Connection");
             e.printStackTrace();
-        } finally
-
-        {
-
-            try {
-
-                if (pst != null) {
-                    pst.close();
-                }
-
-                if (connection != null) {
-                    connection.close();
-                }
-
-            } catch (SQLException ex) {
-
-                Logger lgr = Logger.getLogger(Main.class.getName());
-                lgr.log(Level.SEVERE, ex.getMessage(), ex);
-            }
         }
     }
 
+    public void close() {
+        try {
+
+            if (preparedStatement != null) {
+                preparedStatement.close();
+            }
+
+            if (connection != null) {
+                connection.close();
+            }
+
+        } catch (SQLException e) {
+            System.out.println("Failed to Close Database Connection");
+            e.printStackTrace();
+        }
+    }
+
+    // Show All
+    public ResultSet query(String sql) {
+
+        ResultSet resultSet = null;
+
+        try {
+            preparedStatement = connection.prepareStatement(sql);
+            resultSet = preparedStatement.executeQuery();
+
+        } catch (Exception e) {
+            System.out.println("Query Execution Failed");
+            e.printStackTrace();
+        }
+        return resultSet;
+    }
+
+    //Add or Delete
+    public boolean update(String sql) {
+        boolean flag = false;
+
+        try {
+            preparedStatement = connection.prepareStatement(sql);
+            preparedStatement.executeUpdate();
+            flag = true;
+        } catch (Exception e) {
+            System.out.println("Update Execution Failed");
+            e.printStackTrace();
+        }
+        return flag;
+
+    }
 }
+
+
 
