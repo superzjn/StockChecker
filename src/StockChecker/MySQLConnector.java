fb7fda4e1804b5ed84c2ed9ceead5d2a7079ef00
superzjn <superzjn@gmail.com>
2016-08-31 11:44:01 -0500

    Database initialization fix

--- src/StockChecker/MySQLConnector.java
+++ src/StockChecker/MySQLConnector.java
@@ -5,27 +5,37 @@ import java.sql.*;
 /**
  * Created by jzhang9 on 8/18/2016.
  */
-public class MySQLConnector {
+class MySQLConnector {
 
     private Connection connection = null;  // Database connection
     private PreparedStatement preparedStatement = null;  // SQL statement
 
     private static final String driverName = "com.mysql.jdbc.Driver";
-    private static final String url = "jdbc:mysql://localhost:3306/stockchecker";  // Database location
+    private static final String url = "jdbc:mysql://localhost:3306/";  // Database location
     private static final String user = "root";      // username
     private static final String password = "root";   // password
 
-    public MySQLConnector() {
+    MySQLConnector() {
         try {
             Class.forName(driverName);//  Specify connection type
             connection = DriverManager.getConnection(url, user, password);
+
+            preparedStatement = connection.prepareStatement("CREATE DATABASE IF NOT EXISTS stockchecker;");
+            preparedStatement.execute();
+
+            preparedStatement = connection.prepareStatement("USE stockchecker;");
+            preparedStatement.execute();
+
+            preparedStatement = connection.prepareStatement("CREATE TABLE IF NOT EXISTS watchlist (Urls VARCHAR(150), AddDate TIMESTAMP, PRIMARY KEY (Urls));");
+            preparedStatement.execute();
+
         } catch (Exception e) {
             System.out.println("Failed to Establish Databse Connection");
             e.printStackTrace();
         }
     }
 
-    public void close() {
+    void close() {
         try {
 
             if (preparedStatement != null) {
@@ -43,7 +53,7 @@ public class MySQLConnector {
     }
 
     // Show All
-    public ResultSet query(String sql) {
+    ResultSet query(String sql) {
 
         ResultSet resultSet = null;
 
@@ -59,7 +69,7 @@ public class MySQLConnector {
     }
 
     //Add or Delete
-    public int update(String sql) {
+    int update(String sql) {
         int flag = 0;
 
         try {
