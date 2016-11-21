a881d2bd59708aa51aec7ba39907bc46a81ff739
superzjn <superzjn@gmail.com>
2016-11-21 16:08:32 -0600

    Bug fix, cannot read website info from redsgear. Getting 403 error. Add userAgent to fix it

--- src/StockChecker/websites/Website.java
+++ src/StockChecker/websites/Website.java
@@ -20,7 +20,7 @@ public abstract class Website {
 
     Website(String url) {
         this.url = url;
-        this.httpConn = Jsoup.connect(url).timeout(40000);
+        this.httpConn = Jsoup.connect(url).userAgent("Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/33.0.1750.152 Safari/537.36").timeout(40000);
 
     }
 
@@ -36,7 +36,7 @@ public abstract class Website {
     Document getDoc() throws SocketTimeoutException {   //Get webpage content
 
         try {
-            doc = this.httpConn.get();
+            doc = this.httpConn.userAgent("Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/33.0.1750.152 Safari/537.36").get();
         } catch (SocketTimeoutException e) {
             e.printStackTrace();
             try {
