bfa774b76670c5e23d6a76c81e8d2d073e4f37a0
superzjn <superzjn@gmail.com>
2017-01-20 14:29:46 -0600

    Fix amazon oos Update jsoup and mysql connector

--- src/StockChecker/websites/Amazon.java
+++ src/StockChecker/websites/Amazon.java
@@ -36,13 +36,13 @@ public class Amazon extends Website {
         } catch (SocketTimeoutException e) {
             System.out.println("Time out catch from website sub class");
         }
-
-        // 2 kinds of OOS message on amazon
+        System.out.println(doc.toString());
+        // 3 kinds of OOS message on amazon
         Elements oosMessage = doc.getElementsMatchingText(Pattern.compile("Currently unavailable"));
-
         Elements oosMessage2 = doc.getElementsMatchingText(Pattern.compile("Temporarily out of stock."));
+        Elements oosMessage3 = doc.getElementsMatchingText(Pattern.compile("Usually ships within"));
 
-        return oosMessage.size() != 0 || oosMessage2.size() != 0;   //return true if the message exists
+        return oosMessage.size() != 0 || oosMessage2.size() != 0 ||oosMessage3.size() != 0;   //return true if the message exists
     }
 }
 
